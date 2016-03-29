6d2056069ef542ec6512af8212d120bf474d1b39
SmetDenis <denis@jbzoo.com>
2016-03-29 20:32:06 +0700

    Code style

--- tests/CodestyleTest.php
+++ tests/CodestyleTest.php
@@ -15,6 +15,8 @@
 
 namespace JBZoo\PHPUnit;
 
+use Symfony\Component\Finder\Finder;
+
 /**
  * Class CodestyleTest
  * @package JBZoo\PHPUnit
@@ -24,4 +26,69 @@ class CodestyleTest extends Codestyle
     protected $_packageName = "Less";
     protected $_packageAuthor = 'Denis Smetannikov <denis@jbzoo.com>';
 
+    /**
+     * Ignore list for
+     * @var array
+     */
+    protected $_excludePaths = array(
+        '.git',
+        '.idea',
+        'bin',
+        'bower_components',
+        'build',
+        'fonts',
+        'logs',
+        'node_modules',
+        'resources',
+        'vendor',
+
+        // Only for JBZoo/Less
+        'expected-gpeasy',
+        'expected-iless',
+        'expected-leafo-pseudo',
+        'expected-leafo-real',
+    );
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testHeadersCSS()
+    {
+        $valid = $this->_prepareTemplate(implode($this->_validHeaderCSS, $this->_le));
+
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->in(PROJECT_ROOT)
+            ->exclude($this->_excludePaths)
+            ->name('*.css')
+            ->notName('*_less.css') // Only for JBZoo/Less
+            ->notName('*.min.css');
+
+        /** @var \SplFileInfo $file */
+        foreach ($finder as $file) {
+            $content = openFile($file->getPathname());
+            isContain($valid, $content, false, 'File has no valid header: ' . $file);
+        }
+    }
+
+    /**
+     * Test line endings
+     */
+    public function testFiles()
+    {
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->in(PROJECT_ROOT)
+            ->notName('*_less.css') // Only for JBZoo/Less
+            ->exclude($this->_excludePaths);
+
+        /** @var \SplFileInfo $file */
+        foreach ($finder as $file) {
+            $content = openFile($file->getPathname());
+            isNotContain("\r", $content, false, 'File has \r symbol: ' . $file);
+            isNotContain("\t", $content, false, 'File has \t symbol: ' . $file);
+        }
+    }
 }
